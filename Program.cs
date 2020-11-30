4bc1958e9fdd818c2822b6fa781cdfa8859fb516
muimui123 <71203001+muimui123@users.noreply.github.com>
2020-11-29 17:06:40 -0700

    Add files via upload
    File Input

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1){
+                Console.WriteLine("Sorry, could not find ");
+            } else{
+                //ask filename
+                string filename = args[0];
+                int n = 0;
+                employee[] list = null;
+                //try exception
+                try
+                {
+                    var sr = new StreamReader(filename);
+                    string firstLine = sr.ReadLine();
+                    n = int.Parse(firstLine);
+                    list = new employee[n];
+
+                    for (int i = 0; i < n; i++)
+                    {
+                        string nextLine = sr.ReadLine();
+                        list[i] = new employee(nextLine);
+                    }
+                    sr.Close();
+
+                }
+                catch (FileNotFoundException e)
+                {
+                    Console.WriteLine("Sorry, could not find " + filename);
+                }
+
+                //write lines
+                StreamWriter sw = new StreamWriter("k");
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine(list[i]);
+                    sw.WriteLine(list[i]);
+                }
+                sw.Close();
+
+            }
+        }
+    }
+}
---
+++ employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class employee
+    {
+        //fields
+        private string name;
+        private int id;
+        private int age;
+        private string job;
+        private int year;
+
+
+        //instantiate
+        public employee(string data)
+        {
+            string[] fields = data.Split('|');
+            name = fields[0];
+            id = int.Parse(fields[1]);
+            age = int.Parse(fields[2]);
+            job = fields[3];
+            year = int.Parse(fields[4]);
+        }
+
+        //print
+        public override string ToString()
+        {
+            return "Name:" + name + "    ID:" + id + "    Age:" + age +
+                "   Job:" + job + "  Year:" + year;
+        }
+
+    }
+}
